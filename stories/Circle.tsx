94fbc4e0812d0ca97fb660f705d37b89a954b4f1
kitoumasaomi <kanfutrooper@gmail.com>
2025-12-01 20:56:32 +0900

    Circle.tsxを作成

--- stories/Circle.tsx
+++ stories/Circle.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
-const Circle = () => {
-  return <div className='w-14 h-14 p-14 bg-red-500 rounded-full'></div>;
+type Props = {
+  variant: 'orange' | 'green' | 'yellow';
+};
+
+const Circle = ({ variant }: Props) => {
+  let bgColor;
+
+  switch (variant) {
+    case 'orange':
+      bgColor = 'bg-orange-500';
+      break;
+    case 'green':
+      bgColor = 'bg-green-500';
+      break;
+    case 'yellow':
+      bgColor = 'bg-yellow-500';
+      break;
+  }
+  return (
+    <div className={`${bgColor} w-14 h-14 p-14 bg-red-500 rounded-full`}></div>
+  );
 };
 
 export default Circle;
